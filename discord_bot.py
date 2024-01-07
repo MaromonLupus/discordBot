070f375288fac2e6ef5576e2abf12bfcaf6d4c61
Marek <lupusmaromon@gmail.com>
2024-01-07 11:48:50 +0100

    Update from requirements and cleaning code

--- discord_bot.py
+++ discord_bot.py
@@ -1,15 +1,12 @@
 import disnake
 from disnake.ext import commands, tasks
 
-from config import BOT_TOKEN  # Import the configuration settings
+from config import BOT_TOKEN
 import music_commands
 import open_ai_commands
 
-# Define intents for the bot
 intents = disnake.Intents.default()
-intents.message_content = True  # Enable message content intent for prefix commands
-
-# Define the bot instance with command prefix and intents
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @tasks.loop(minutes=800)
@@ -19,7 +16,6 @@ async def check_inactivity():
     """
     for vc in bot.voice_clients:
         if not vc.is_playing() and not vc.is_paused():
-            # Disconnect the bot if it's alone in the voice channel
             if len(vc.channel.members) == 1:
                 await vc.disconnect()
 
@@ -37,11 +33,9 @@ async def on_ready():
     """
     print(f'We have logged in as {bot.user}')
 
-# Setup the commands with the bot instance
 music_commands.setup(bot)
 open_ai_commands.setup(bot)
-# Start the inactivity check loop
+
 check_inactivity.start()
 
-# Run the bot with the provided token
 bot.run(BOT_TOKEN)
